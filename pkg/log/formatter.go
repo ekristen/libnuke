6d82056ddbbf494375b9f41bdc738e4580e60289
Erik Kristensen <erik@erikkristensen.com>
2024-10-06 00:24:16 -0600

    fix: delete _handler

--- pkg/log/formatter.go
+++ pkg/log/formatter.go
@@ -23,6 +23,7 @@ func (f *CustomFormatter) Format(entry *logrus.Entry) ([]byte, error) { //nolint
 
 	handler, ok := entry.Data["_handler"].(string)
 	if ok && handler == "println" {
+		delete(entry.Data, "_handler")
 		return []byte(entry.Message), nil
 	}
 
