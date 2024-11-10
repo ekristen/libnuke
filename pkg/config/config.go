0ce52940b24de951a8692e88db73101b778ed2d7
Erik Kristensen <erik@erikkristensen.com>
2024-11-10 08:22:51 -0700

    fix: nil pointer on incomplete config for accounts

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -236,6 +236,10 @@ func (c *Config) Filters(accountID string) (filter.Filters, error) {
 // new resource type.
 func (c *Config) ResolveDeprecations() error {
 	for _, a := range c.Accounts {
+		if a == nil {
+			return nil
+		}
+
 		// Note: if there are no filters defined, then there's no substitution to perform.
 		if a.Filters == nil {
 			return nil
